c791c1726996709afd34a4c8a0b4a9a8677663fd
Ammar <amaar.alaa93@gmail.com>
2021-09-24 23:23:34 +0300

    last last3

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9o)37&w8q-urdxhi$y+k7j&2a*mk_$p%*hxs2!!ot#tqa#s7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
